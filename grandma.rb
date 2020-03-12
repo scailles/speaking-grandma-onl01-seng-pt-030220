93373e1302e4eae2c3480a1dc86075f3157d9246
Shaina C <scailles23@gmail.com>
2020-03-12 05:40:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,6 @@
 
 def speak_to_grandma(string)
  if string.upcase=false
-   return "HUH?! SPEAK UP, SONNY!"
-   elsif phrase="I LOVE YOU GRANDMA"
-   return "I LOVE YOU TOO PUMPKIN!"
- end
- end
- end
+ 
 
   
